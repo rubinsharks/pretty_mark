7d04538970cca2a6871106d2bfdf6aa5c236bd1b
Rubin Yeom <rubinyeom@Rubinui-MacBookAir.local>
2024-05-21 22:00:20 +0900

    auto generate index

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    let path = Path::new("root/index.md");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => {}
+    }
+
+    println!("{}", markdown::to_html(&s));
+
+    let html = markdown::to_html(&s);
+
+    let mut file = File::create("index.html")?;
+    file.write_all(html.as_bytes())?;
+    Ok(())
 }

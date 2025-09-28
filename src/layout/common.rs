fda8c1ac474f8cd2c06dfa85a55e9957d14bf705
Rubin Yeom <rubinsharks@gmail.com>
2025-09-28 06:38:43 +0000

    error fix

--- src/layout/common.rs
+++ src/layout/common.rs
@@ -78,7 +78,7 @@ pub fn layout_to_tomlview(view: &dyn TOMLView, layout: String, layout_tables: Ha
 }
 
 pub fn get_layout_tables_except_key(index_path: &Path, except_key: &str) -> Result<HashMap<String, Table>, String> {
-    let mut file = File::open(index_path).ok().ok_or(format!("Failed to open the file in layout_tables in {:?}", index_path)?;
+    let mut file = File::open(index_path).ok().ok_or(format!("Failed to open the file in layout_tables in {:?}", index_path))?;
 
     let mut config_str = String::new();
     file.read_to_string(&mut config_str)
